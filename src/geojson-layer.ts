9bcc99d59850e9d47a088e9fe5dff584c349f6e6
Amit Assaraf <amit@helpi.me>
2017-11-27 19:37:02 +0200

    Fixed source issue

--- src/geojson-layer.ts
+++ src/geojson-layer.ts
@@ -245,7 +245,10 @@ export default class GeoJSONLayer extends React.Component<Props> {
     const { map } = this.context;
 
     if (props.data !== data) {
-      (map.getSource(this.id) as MapboxGL.GeoJSONSource).setData(props.data);
+      const source = (map.getSource(this.id) as MapboxGL.GeoJSONSource);
+      if (source) {
+        source.setData(props.data);
+      }
 
       this.source = {
         type: 'geojson',
